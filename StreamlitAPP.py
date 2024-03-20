d40a69ba25c58f3f48ccf6650da2d917a10dc5a1
anishgupta02 <yamaanishgupta@gmail.com>
2024-03-21 02:24:46 +0530

    updated StreamlitAPP.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -13,7 +13,7 @@ from src.mcqgenerator.logger import logging
 
 
 #load the response
-with open('F:\mcqgen\Response.json','r') as file:
+with open('Response.json','r') as file:
     RESPONSE_JSON=json.load(file)
 
 #creating a title for the app
